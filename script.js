d84fc9d8111140e86ffe3dee92118e6a9b62b44d
boas <boassyonathan@gmail.com>
2024-03-18 12:07:29 +0700

    v1.3 - Menambahkan operasi kali

--- script.js
+++ script.js
@@ -12,4 +12,9 @@ function kurang() {
     document.getElementById("result").innerHTML = num1 - num2;
 }
 
-
+function kali() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 * num2;
+}
