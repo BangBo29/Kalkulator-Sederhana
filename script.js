dd6869afa1a93462f321fdbf6db9002195c4b192
boas <boassyonathan@gmail.com>
2024-03-18 11:52:10 +0700

    v1.0 - Menambahkan operasi tambah

---
+++ script.js
@@ -0,0 +1,34 @@
+function tambah() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function kurang() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function kali() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function bagi() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function modulus() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 % num2;
+}
