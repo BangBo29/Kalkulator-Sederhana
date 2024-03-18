85ab1bc9780b37693132149ddcd796bd36ec9c32
boas <boassyonathan@gmail.com>
2024-03-18 12:05:48 +0700

    v1.1 - Menambahkan operasi kurang

---
+++ script.js
@@ -0,0 +1,15 @@
+function tambah() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function kurang() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 - num2;
+}
+
+
