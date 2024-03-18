87f411465e7fff2cc2ecc4f249465ad2dfd3624a
boas <boassyonathan@gmail.com>
2024-03-18 12:09:20 +0700

    v1.5 - Menambahkan operasi modulus

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ function bagi() {
     document.getElementById("hasil").textContent = "Hasil:";
     document.getElementById("result").innerHTML = num1 / num2;
 }
+
+function modulus() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 % num2;
+}
