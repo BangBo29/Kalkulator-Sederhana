29750288386ba60157266215cd011735ea466080
boas <boassyonathan@gmail.com>
2024-03-18 12:08:26 +0700

    v1.4 - Menambahkan operasi bagi

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ function kali() {
     document.getElementById("hasil").textContent = "Hasil:";
     document.getElementById("result").innerHTML = num1 * num2;
 }
+
+function bagi() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    document.getElementById("hasil").textContent = "Hasil:";
+    document.getElementById("result").innerHTML = num1 / num2;
+}
